b58256f6d2fdc0bd7a6671af350635e11ebfdc35
annieogrady <annieogrady22@gmail.com>
2013-12-27 14:22:44 -0500

    add gravatar functionality

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -82,7 +82,7 @@ module ApplicationHelper
   
   def avatar_url(user)
     default_url = "#{root_url}images/guest_png"
-    gravatar_id = Digest :: MD5.hexdigest(user.email.downcase)
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
      "http://gravatar.com/avatar/#{gravatar_id}.png?s=48d=#{CGI.escape(default_url)} " 
   end 
 
